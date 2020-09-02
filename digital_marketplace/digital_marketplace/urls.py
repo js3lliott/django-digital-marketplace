12f06d21825c657e2301014944b4f5e10d436f3d
Jordan Samek <jordansamek@Jordans-MacBook-Pro.local>
2020-09-02 16:13:12 -0400

    Created first view, added book-list.html

--- digital_marketplace/digital_marketplace/urls.py
+++ digital_marketplace/digital_marketplace/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('books.urls', namespace='books'))
 ]
 
 if settings.DEBUG:
