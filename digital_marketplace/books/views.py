1948035404becfb86b6b0c3f2fe0524acadbc649
Jordan Samek <jordansamek@Jordans-MacBook-Pro.local>
2020-09-03 15:52:29 -0400

    Created exercise detail view and exercise html page

--- digital_marketplace/books/views.py
+++ digital_marketplace/books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Chapter
+from .models import Book, Chapter, Exercise
 from django.http import Http404
 
 
@@ -20,10 +20,23 @@ def book_detail(request, slug):
     return render(request, "book_detail.html", context)
 
 def chapter_detail(request, book_slug, chapter_number):
+    # display a list of the exercises in the chapter
     chapter_qs = Chapter.objects.filter(book__slug=book_slug).filter(chapter_number=chapter_number)
     if chapter_qs.exists():
         context = {
             'chapter': chapter_qs[0]
         }
         return render(request, "chapter_detail.html", context)
+    return Http404
+
+def exercise_detail(request, book_slug, chapter_number, exercise_number):
+    exercise_qs = Exercise.objects \
+        .filter(chapter__book__slug=book_slug) \
+        .filter(chapter__chapter_number=chapter_number) \
+        .filter(exercise_number=exercise_number)
+    if exercise_qs.exists():
+        context = {
+            'exercise': exercise_qs[0]
+        }
+        return render(request, "exercise_detail.html", context)
     return Http404
