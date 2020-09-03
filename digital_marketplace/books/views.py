76a27353d6275e476f2d71d751fc8d0d8d2745b0
Jordan Samek <jordansamek@Jordans-MacBook-Pro.local>
2020-09-03 15:40:11 -0400

    Added book detail, chapter detail views along with book and chapter html pages

--- digital_marketplace/books/views.py
+++ digital_marketplace/books/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Chapter
+from django.http import Http404
 
 
 def book_list(request):
+    # display a list of all the books
     queryset = Book.objects.all()
     context = {
         'queryset': queryset
     }
     return render(request, "book_list.html", context)
+
+def book_detail(request, slug):
+    # display a list of the chapters in this book
+    book = get_object_or_404(Book, slug=slug)
+    context = {
+        'book': book
+    }
+    return render(request, "book_detail.html", context)
+
+def chapter_detail(request, book_slug, chapter_number):
+    chapter_qs = Chapter.objects.filter(book__slug=book_slug).filter(chapter_number=chapter_number)
+    if chapter_qs.exists():
+        context = {
+            'chapter': chapter_qs[0]
+        }
+        return render(request, "chapter_detail.html", context)
+    return Http404
